9f527663b08554c9b40ff4d9b8a1ed0a8d7acb29
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2025-07-06 23:04:39 -0400

    some logic was added

--- src/main.rs
+++ src/main.rs
@@ -12,28 +12,64 @@ struct Bank {
 
 impl Bank {
     fn new() -> Self {
-        Bank {accounts: vec![]}
+        Bank { accounts: vec![] }
+    }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|a| a.balance).sum()
+    }
+
+    fn account_summary(&self) -> Vec<String> {
+        self.accounts.iter().map(|a| a.summary()).collect()
     }
 }
 
 impl Account {
     fn new(id: u32, holder: String) -> Self {
-        Account { id: id, balance: 0, holder: holder }
+        Account {
+            id: id,
+            balance: 0,
+            holder: holder,
+        }
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        if amount > self.balance {
+            panic!("Insufficient funds for withdrawal");
+        } else {
+            self.balance -= amount;
+            self.balance
+        }
+    }
+
+    fn summary(&self) -> String {
+        format!(
+            "Account ID: {}, Holder: {}, Balance: {}",
+            self.id, self.holder, self.balance
+        )
     }
 }
 
-
-fn print_account(account: Account) {
-    println!("Account: {:#?}", account);
-}
 fn main() {
+    let mut account1 = Account::new(1, String::from("Alice"));
+    let mut account2 = Account::new(2, String::from("Dan"));
+
+    account1.deposit(100);
+    account2.deposit(200);
+    account2.withdraw(32);
     let mut bank = Bank::new();
-    let account1 = Account::new(1, String::from("Alice"));
-    // let account2 = Account::new(2, String::from("Bob"));
-    // bank.accounts.push(account1);
-    // bank.accounts.push(account2);
-    // println!("{:#?}", bank);
-    // println!("Account 1: {:#?}", account1);
-    print_account(account1);
-    print_account(account1);
+    bank.add_account(account1);
+    bank.add_account(account2);
+
+    println!("Total balance in bank: {}", bank.total_balance());
+    println!("Account summaries: {:#?}", bank.account_summary());
 }
